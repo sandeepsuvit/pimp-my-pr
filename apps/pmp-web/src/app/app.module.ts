8f76aef0e76d0a4667d46e37f651e2ba65016fbb
Maciej Sikorski <maciejsikorski00@gmail.com>
2020-01-24 16:57:37 +0100

    feat(pmp-web): add core module

--- apps/pmp-web/src/app/app.module.ts
+++ apps/pmp-web/src/app/app.module.ts
@@ -3,39 +3,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PmpWebRepositoryShellModule } from '@pimp-my-pr/pmp-web/repository/shell';
 import { PmpWebUserShellModule } from '@pimp-my-pr/pmp-web/user/shell';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
-import { EffectsModule } from '@ngrx/effects';
-import { NxModule } from '@nrwl/angular';
-import { StoreModule } from '@ngrx/store';
-import { environment } from '../environments/environment';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { PmpWebCoreModule } from '@pimp-my-pr/pmp-web/core';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    BrowserModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
     RouterModule.forRoot([]),
-    EffectsModule.forRoot([]),
-    NxModule.forRoot(),
-    StoreModule.forRoot(
-      {},
-      {
-        metaReducers: !environment.production ? [] : [],
-        runtimeChecks: {
-          strictActionImmutability: true,
-          strictStateImmutability: true
-        }
-      }
-    ),
-    StoreDevtoolsModule.instrument({
-      maxAge: 25, // Retains last 25 states
-      logOnly: environment.production // Restrict extension to log-only mode
-    }),
+    PmpWebCoreModule,
     PmpWebRepositoryShellModule,
     PmpWebUserShellModule
   ],

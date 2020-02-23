3facb27c792e098ecc653decc193150dd5ec83dc
Maciej Sikorski <maciejsikorski00@gmail.com>
2020-02-23 21:50:20 +0100

    test(pmp-api): add api e2e

--- apps/pmp-api-e2e/integration/app.spec.ts
+++ apps/pmp-api-e2e/integration/app.spec.ts
@@ -1,35 +1,56 @@
-import * as request from 'supertest';
-import { Test } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-//  TODO to remove, disable only to show proposal of usage
-//  tslint:disable-next-line
-import { AppModule } from '../../pmp-api/src/app/app.module';
+import { Test } from '@nestjs/testing';
+import { ServerRepositoryApiRestModule } from '@pimp-my-pr/server/repository/api-rest';
+import { RepositoryFacade } from '@pimp-my-pr/server/repository/core/application-services';
+import { ServerSharedCoreModule } from '@pimp-my-pr/server/shared/core';
+import * as request from 'supertest';
 
-describe('Mockup', () => {
-  it('empty test', () => {
-    expect(true).toBe(true);
+describe('Application', () => {
+  let app: INestApplication;
+  const repositoryFacade = {
+    getRepositoryStatistics: params => [params],
+    getReviewerStatistics: params => [params],
+    listRepositoriesStatistics: () => ['repositories'],
+    listReviewersStatistics: () => ['reviewers']
+  };
+
+  beforeAll(async () => {
+    const module = await Test.createTestingModule({
+      imports: [ServerRepositoryApiRestModule, ServerSharedCoreModule]
+    })
+      .overrideProvider(RepositoryFacade)
+      .useValue(repositoryFacade)
+      .compile();
+
+    app = module.createNestApplication();
+    await app.init();
+  });
+
+  test(`/GET repositories statistics`, () => {
+    return request(app.getHttpServer())
+      .get('/repository')
+      .expect(200, { data: ['repositories'], error: null });
+  });
+
+  test(`/GET reviewers statistics`, () => {
+    return request(app.getHttpServer())
+      .get('/reviewers')
+      .expect(200, { data: ['reviewers'], error: null });
+  });
+
+  test(`/GET repository statistics`, () => {
+    return request(app.getHttpServer())
+      .get('/repository/1234123')
+      .expect(200, { data: [{ repositoryId: '1234123' }], error: null });
+  });
+
+  test(`/GET reviewer statistics`, () => {
+    return request(app.getHttpServer())
+      .get('/reviewers/username')
+      .expect(200, { data: [{ username: 'username' }], error: null });
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
-
-// describe('Application', () => {
-//   let app: INestApplication;
-//
-//   beforeAll(async () => {
-//     const module = await Test.createTestingModule({
-//       imports: [AppModule]
-//     }).compile();
-//
-//     app = module.createNestApplication();
-//     await app.init();
-//   });
-//
-//   it(`/GET hello`, () => {
-//     return request(app.getHttpServer())
-//       .get('/hello')
-//       .expect(200, 'Welcome to api!');
-//   });
-//
-//   afterAll(async () => {
-//     await app.close();
-//   });
-// });
